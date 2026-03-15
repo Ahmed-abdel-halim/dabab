fadb2744e75e6b901ce5c9525808d3d6f237be7c
Ahmed-abdel-halim <ahmedholim42@gmail.com>
2026-03-15 17:39:46 +0200

    edit

--- app/Models/DeliveryVehicle.php
+++ app/Models/DeliveryVehicle.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class DeliveryVehicle extends Model
 {
-    //
+    protected $fillable = [
+        'delivery_agent_profile_id',
+        'vehicle_type',
+        'vehicle_brand',
+        'vehicle_model',
+        'manufacturing_year',
+        'license_plate_number',
+        'license_plate_letters',
+        'license_type',
+    ];
 }
